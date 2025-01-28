4e0448d516b7b0e98870106efff7ca4454a8f09f
Siddhesh <b22cs016@iitj.ac.in>
2025-01-28 06:56:40 +0530

    Create and Delete api's tested & successfully working

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';    
-
+import Product from './models/product.model.js';  // Add this line to import the Product model
 dotenv.config();
 
 const app = express();
@@ -30,6 +30,16 @@ app.post("/api/products", async (req,res) =>{
 
 })
 
+app.delete("/api/products/:id", async(req,res) =>{
+    const {id} = req.params;
+try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({success: true, message:"Product deleted"});
+} catch (error){
+    res.status(404).json({success: false, message: "Product not found"});
+
+}
+});
 
 
 app.listen(5000, () => {
